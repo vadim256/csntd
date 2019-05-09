26b0ab5d1b7d81b5ad0eeb042f3ab95a0135036e
Vadim <kvadiml256@gmail.com>
2019-05-09 12:10:59 +0300

    видалено сміття з файла

--- container_ip.c
+++ container_ip.c
@@ -30,69 +30,7 @@ void AddList(ListIP ** u, const Data x){
     p->next = *u;
     *u = p;
 }
-/*
-void Insert(ListIP ** u, const Data  x){
-    ListIP *p = (ListIP*) malloc(sizeof(ListIP));
-    if(!p)
-        return;
-    p->d.address_ip = (char *) malloc(sizeof(char)*strlen(x->address_ip)+1);
-    if(!(p->d.address_ip))
-        return;
-    strcpy(p->d.address_ip, x->address_ip);
-    p->d.count_ip = x->count_ip;
-    if(*u == 0) 
-    {
-        p->next = 0;
-        *u = p;
-      return;
-    }
-    ListIP *t = *u;
-    if(strcmp(t->d.address_ip, p->d.address_ip) >= 0) {
-        p->next = t;
-        *u = p;
-        return;
-    }
-    ListIP *t1 = t->next;
-    while(t1) {
-        if(strcmp(t->d.address_ip, p->d.address_ip) < 0 && 
-           strcmp(p->d.address_ip, t1->d.address_ip) <= 0) { 
-            t->next = p;
-            p->next = t1;
-            return;
-        }
-        t = t1;
-        t1 = t1->next;
-    }
-    t->next = p; 
-    p->next = 0;
-}
 
-void Delete(ListIP ** u, const Data * x){
-    if(*u == 0){
-        return;
-    }
-    ListIP *t = *u;
-    if(!strcmp(t->d.address_ip,x->address_ip)) {
-        *u = t->next;
-        free(t->d.address_ip);
-        free(t);
-        return;
-    }
-
-    ListIP *t1 = t->next;
-
-    while(t1) {
-        if(!strcmp(t1->d.address_ip, x->address_ip)){
-            t->next = t1->next;
-            free(t1->d.address_ip);
-            free(t1);
-            return;
-        }
-        t = t1;
-        t1 = t1->next;
-   }
-}
-*/
 void Clear(ListIP ** u){
 
     if(*u == 0) return;
